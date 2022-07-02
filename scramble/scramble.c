30a48187c82bfe920569551772d02e78b879a49d
ogonna-anaekwe <ogonna.anaekwe@gmail.com>
2022-07-02 19:49:05 -0400

    fix bug w/ randomness

--- scramble/scramble.c
+++ scramble/scramble.c
@@ -1,20 +1,21 @@
 #include "scramble.h"
 
-char *scramble(const char *string)
+char *scramble(const char *str)
 {
     srand(time(NULL)); // seeding w/ current time guarantees randomness. if you used a constant seed, you can get repeatable/deterministic results
-    int str_len = strlen(string),
+    int str_len = strlen(str),
         str_cut = 2; // for the first and last char
-    char *scrambled_string = malloc(str_len);
-    scrambled_string[0] = string[0];
-    scrambled_string[str_len - 1] = string[str_len - 1];
+    char *scrambled_str = malloc(str_len);
+    scrambled_str[0] = str[0];
+    scrambled_str[str_len - 1] = str[str_len - 1];
 
-    int inner_str = (str_len - str_cut),
-        str_idx = 1,
+    int substr_len = str_len - str_cut,
+        int_bytes = sizeof(int),
+        scrambled_str_idx = 1,
         random_number,
         value,
         *tmp_arr_two,
-        *tmp_arr_one = malloc(sizeof(int) * inner_str);
+        *tmp_arr_one = malloc(int_bytes * substr_len);
 
     if (!tmp_arr_one)
     {
@@ -22,19 +23,18 @@ char *scramble(const char *string)
         exit(1);
     }
 
-    for (int *i = tmp_arr_one; i < tmp_arr_one + inner_str; i++)
+    for (int *i = tmp_arr_one; i < tmp_arr_one + substr_len; i++)
     {
         tmp_arr_one[i - tmp_arr_one] = (int)(i - tmp_arr_one);
     }
 
-    while (inner_str > 1)
+    while (substr_len > 1)
     {
-        inner_str--;
-        random_number = 1 + (rand() % inner_str); // this will be between 1 and inner_str so we can select any of the chars btw the first and last. w/o the 1, we'll always fail to select the char before the last
+        random_number = rand() % substr_len;
         value = tmp_arr_one[random_number];
-        scrambled_string[str_idx] = string[value + 1];
+        scrambled_str[scrambled_str_idx] = str[value + 1];
 
-        tmp_arr_two = malloc(sizeof(int) * inner_str);
+        tmp_arr_two = malloc(int_bytes * substr_len);
         if (!tmp_arr_two)
         {
             fprintf(stderr, "Malloc failed\n");
@@ -44,7 +44,7 @@ char *scramble(const char *string)
         int idx = 0;
         tmp_arr_one[random_number] = -1;
 
-        for (int *i = tmp_arr_one; i <= tmp_arr_one + inner_str; i++)
+        for (int *i = tmp_arr_one; i <= tmp_arr_one + substr_len; i++)
         {
             if (*i >= 0)
             {
@@ -53,12 +53,13 @@ char *scramble(const char *string)
             }
         }
 
-        tmp_arr_one = realloc(tmp_arr_two, sizeof(int) * inner_str);
-        str_idx++;
+        scrambled_str_idx++;
+        substr_len--;
+        tmp_arr_one = realloc(tmp_arr_two, int_bytes * substr_len);
     }
 
-    scrambled_string[str_idx] = string[tmp_arr_two[0] + 1];
+    scrambled_str[scrambled_str_idx] = str[tmp_arr_two[0] + 1];
 
     free(tmp_arr_one);
-    return scrambled_string;
+    return scrambled_str;
 }
--- scramble/scramble.h
+++ scramble/scramble.h
@@ -8,8 +8,8 @@
 
 /**
  * Scrambles a string: randomly repositions all characters but the first and last.
- * @param string String to be scrambled
- * @return char* Scrambled string
+ * @param string String to be scrambled.
+ * @return char* Scrambled string.
  */
 char *scramble(const char *string);
 
@@ -17,10 +17,10 @@ char *scramble(const char *string);
  * Algorithm:
  * 1. Create an array to hold the number of chars btw the first and last. e.g. for "bread", this array will be {0, 1, 2} for 'r', 'e', and 'a'
  * 2. Malloc a new string to hold the scrambled string (the first and last chars of this string matches the original string)
- * 3. Select a random char btw the first and last i.e. btw 0 and 2. If 0 is selected, then 0 + 1 gives 'r' in the original string. We place this value in index 1
- * of the new string. If 2, then 2 + 1 gives the second e, and we place this char in index 1 of the new string instead.
+ * 3. Select a random char btw the first and last i.e. btw 0 and 2 (inclusive). If 0 is selected, then 0 + 1 gives 'r' in the original string. We place this value in index 1
+ * of the new string. If 2, then 2 + 1 gives 'a', and we place this char in index 1 of the new string instead.
  * 4. Update the value of the selected char in the array to -1.
- * 5. Create a new array with the non-negative values (This ensures the next time we randomly select, we're only selecting a value that has yet to be selected).
+ * 5. Create a new array with the non-negative values (from the original array). (This ensures the next time we randomly select, we're only selecting a value that has yet to be selected.)
  * 6. Repeat this process until there is only one value left in the new array.
  *
  * Note: This program is slightly more complicated because of the need for randomness. So that given 'bread', the program could
