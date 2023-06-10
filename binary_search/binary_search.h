fc5d334a3693935841731f9f036984f546a9be98
ogonna-anaekwe <ogonna.anaekwe@gmail.com>
2023-06-10 10:07:29 -0400

    binary search

--- binary_search/binary_search.h
+++ binary_search/binary_search.h
@@ -3,4 +3,11 @@
 #include <math.h>
 #include <stdbool.h>
 
+/**
+ * REF: https://en.wikipedia.org/wiki/Binary_search_algorithm
+ * @param arr Sorted array in which to search for target value.
+ * @param arr_len Length of sorted array.
+ * @param target Value to be searched for in array.
+ * @return int - Index of value in array. -1 if not in array.
+ */
 int binary_search(int arr[], int arr_len, int target);
