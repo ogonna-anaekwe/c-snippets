ab808c22c5a9787f54a6f6d3dc08457d2a328c64
ogonna-anaekwe <ogonna.anaekwe@gmail.com>
2023-10-30 20:15:38 -0400

    use arg index

--- print_dir/main.c
+++ print_dir/main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "usage: %s <directory>\n", argv[0]);
         exit(1);
     }
-    char *dir_name = argv[argc - 1];
+    char *dir_name = argv[1];
     list_dir(dir_name);
     printf("%d dirs; %d files\n", num_dir, num_files);
     return 0;
